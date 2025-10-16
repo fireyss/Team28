93519000dcb47705800e9e03c0b34396a3c98f2e
fireyss <133407801+fireyss@users.noreply.github.com>
2025-10-16 13:58:11 +0100

    set up framework for nested pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "@/pages/Login";
 import Dashboard from "@/pages/Dashboard";
 
+//dashboard pages
+import Dashboard_Home from "@/pages/dashboard_pages/Home"
+
 export default function AppRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="home" element={<Dashboard_Home />} />
+        </Route>
       </Routes>
     </Router>
   );
