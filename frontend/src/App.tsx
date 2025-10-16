3ecb715a2cbba3c889535fd354574302818daa03
fireyss <133407801+fireyss@users.noreply.github.com>
2025-10-16 17:08:04 +0100

    added light/dark theming

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "@/pages/Login";
 import Dashboard from "@/pages/Dashboard";
+import { ThemeProvider } from "@/components/theme-provider"
 
 //dashboard pages
 import Dashboard_Home from "@/pages/dashboard_pages/Home"
 
 export default function AppRoutes() {
   return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
@@ -17,5 +19,6 @@ export default function AppRoutes() {
         </Route>
       </Routes>
     </Router>
+    </ThemeProvider>
   );
 }
