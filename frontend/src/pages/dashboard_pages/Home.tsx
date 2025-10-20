aadb75b534cf69e219f3b9b11a3a03aadb7c76e1
hoangbenjaminitmb <hoang.benjamin3@gmail.com>
2025-10-20 18:42:31 +0100

    fix

--- frontend/src/pages/dashboard_pages/Home.tsx
+++ frontend/src/pages/dashboard_pages/Home.tsx
@@ -5,6 +5,8 @@ import { SectionCards } from "@/components/section-cards";
 import data from "./data.json";
 
 export default function Home() {
+  
+
   return (
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
       <SectionCards />
