a7be41f63495fb46f2cc2910fd10156ec463e00e
FollyAurelio <folly.aurelio@gmail.com>
2025-10-20 17:31:58 +0200

    Added linking to the sidebar for the todo list

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,22 +3,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Login from "@/pages/Login";
 import Dashboard from "@/pages/Dashboard";
+import Dashboard_Todo from "@/pages/dashboard_pages/Todo-list";
 import { ThemeProvider } from "@/components/theme-provider"
 
 //dashboard pages
 import Dashboard_Home from "@/pages/dashboard_pages/Home"
-import Dashboard_Todo from "@/pages/dashboard_pages/Todo-list"
 
 export default function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route path="home" element={<Dashboard_Home />} />
-          <Route path="todo" element={<Dashboard_Todo />}/>
-        </Route>
-      </Routes>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />}>
+            <Route path="home" element={<Dashboard_Home />} />
+            <Route path="todo" element={<Dashboard_Todo />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
