d905c3bafdf8426a029766c67e13980b49576e78
Rose <rosehardy1234@gmail.com>
2025-10-23 19:20:06 +0100

    Removed Topic page

--- frontend/src/pages/dashboard_pages/Forum.tsx
+++ frontend/src/pages/dashboard_pages/Forum.tsx
@@ -1,13 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import ForumHome from "./forum_pages/Home";
-import TopicPage from "./forum_pages/Topic";
 import PostPage from "./forum_pages/Post";
 
 export default function Forum() {
     return (
         <Routes>
             <Route path="/" element={<ForumHome />} />
-            <Route path="topic/:topicName" element={<TopicPage />} />
             <Route path="post/:postID" element={<PostPage />} />
         </Routes>
     );
--- frontend/src/pages/dashboard_pages/forum_pages/Topic.tsx
+++
@@ -1,8 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export default function TopicPage() {
-    const { topicName } = useParams<{ topicName: string }>();
-    return (
-        <div>{topicName}</div>
-    )
-}
