1d848f9ca8d7600485ef64e87b41f16daf421278
Rose <rosehardy1234@gmail.com>
2025-10-23 18:16:57 +0100

    Made changes to support the forum

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import Login from "@/pages/Login";
 import Dashboard from "@/pages/Dashboard";
-import Dashboard_Todo from "@/pages/dashboard_pages/Todo-list";
 import { ThemeProvider } from "@/components/theme-provider"
 
 //dashboard pages
 import Dashboard_Home from "@/pages/dashboard_pages/Home"
+import Dashboard_Todo from "@/pages/dashboard_pages/Todo-list"
+import Dashboard_Forum from "@/pages/dashboard_pages/Forum"
 
 export default function AppRoutes() {
   return (
@@ -18,6 +19,7 @@ export default function AppRoutes() {
           <Route path="/dashboard" element={<Dashboard />}>
             <Route path="home" element={<Dashboard_Home />} />
             <Route path="todo" element={<Dashboard_Todo />} />
+            <Route path="forum/*" element={<Dashboard_Forum />} />
           </Route>
         </Routes>
       </Router>
