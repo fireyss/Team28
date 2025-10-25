e48956fa4434c38494366d6f3fa2ed62b55af8ae
fireys <rivithan@gmail.com>
2025-10-25 19:04:19 +0100

    Wrapped AuthContext.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "@/components/theme-provider"
+
+
 import Login from "@/pages/Login";
 import Dashboard from "@/pages/Dashboard";
-import { ThemeProvider } from "@/components/theme-provider"
 
 //dashboard pages
 import Dashboard_Home from "@/pages/dashboard_pages/Home"
 
 export default function AppRoutes() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />}>
-            <Route path="home" element={<Dashboard_Home />} />
-          </Route>
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Router>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/dashboard" element={<Dashboard />}>
+              <Route path="home" element={<Dashboard_Home />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
