1008483c822037b8478b1e886ae767face992851
CollarGreen <236860410+CollarGreen@users.noreply.github.com>
2025-10-29 18:52:17 +0000

    please commit

--- frontend/src/pages/dashboard_pages/Home.tsx
+++ frontend/src/pages/dashboard_pages/Home.tsx
@@ -11,7 +11,6 @@ export default function Home() {
     <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
       <SectionCards />
       <div className="px-4 lg:px-6">
-        <ChartAreaInteractive />
         <ChartLineDefault/>
       </div>
       {/* <DataTable data={data} /> */}
