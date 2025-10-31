c5f39c831c5a9fcedf2706538e24dadbc5b80743
FollyAurelio <folly.aurelio@gmail.com>
2025-10-31 19:38:45 +0100

    pie

--- frontend/src/pages/dashboard_pages/Home.tsx
+++ frontend/src/pages/dashboard_pages/Home.tsx
@@ -1,4 +1,5 @@
 import { SectionCards } from "@/components/section-cards";
+import { ChartPieSimple } from "@/components/training-pie-chart";
 
 import {
   IconHome,
@@ -42,6 +43,7 @@ export default function Home() {
         <h1 className="text-[24px]">Welcome back {data.user.name}. We've got some work to do</h1>
       </div>
       <SectionCards />
+      <ChartPieSimple />
     </div>
-  );
+  );  
 }
