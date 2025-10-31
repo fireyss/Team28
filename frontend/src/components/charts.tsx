4d11bce20eeb8971bf7e0797335b30e4db252c0b
CollarGreen <236860410+CollarGreen@users.noreply.github.com>
2025-10-31 01:40:06 +0000

    Start of graphs

--- frontend/src/components/charts.tsx
+++ frontend/src/components/charts.tsx
@@ -1,6 +1,7 @@
 
+
 import { TrendingUp } from "lucide-react"
-import { CartesianGrid, Line, LineChart, XAxis } from "recharts"
+import { CartesianGrid, Line, LineChart, ReferenceLine, XAxis, YAxis } from "recharts"
 
 import {
   Card,
@@ -37,53 +38,140 @@ const chartConfig = {
 
 import projectData from "@/data/projectData.json";
 import taskData from "@/data/taskData.json";
+import { useState } from "react";
+import { int } from "zod";
 
 export function ChartLineDefault() {
-  const project = projectData.find((data) => data.id = 1);
-  console.log(project?.author);
+  //sorting data to get required info for graphs
 
-  const taskList = taskData.filter((task) => task?.project === project?.id);
-  console.log(taskList);
-  console.log(taskList.map((task) => {return {taskTitle: task.title, taskAssignee: task.assignee}} ));
+  //User validation here
+
+
+  let project = projectData.find((data) => data.id === 5); //And user is part of the project
+  console.log(project);
+
+  let taskList = taskData.filter((task) => task?.project === project?.id);
+  let graphData = taskList.map((task) => {return {title: task.title, 
+              assignee: task.assignee, 
+              status: task.status, 
+              urgency: task.urgency,
+              completed: task.completed}} );
+  ///
+  let completedTasks = graphData.filter((data) => data.completed != null);
+  let tasksArray = [
+    {date: project?.posted, completedCount: 0},
+    {date: project?.deadline, completedCount: null},
+  ];
+
+  let completedColour = "#0000ff";
+  let indexSub = 1;
+  if(project?.completed != null){
+      tasksArray.splice(1, 0, {date: project?.completed!, completedCount: graphData.length});
+      tasksArray.find((task) => task.date === project?.deadline)!.completedCount = graphData.length
+      indexSub = 2;
+      completedColour = "#00ff00";
+  }
+  let hi = "11/02/25";
+  let bello = hi.slice(3, 6) + hi.slice(0, 3) + hi.slice(6);
+  console.log(tasksArray);
+  //console.log(bello);
+  //let hello = project?.completed!.slice(3, 6) + project?.completed!.slice(0, 3)! + project?.completed!.slice(6);
+  //console.log(project?.completed!)
+  //console.log(hello)
+
+  console.log(tasksArray);
+
+  let thisTest = [
+    {
+      date: project?.deadline, total: taskList.length
+    },
+    // {
+    //   date: project?.deadline, total: taskList.length
+    // }
+  ]
+  //tasksArray.push({date: completedTasks[0].completed!, completedCount: 1});
+  //for loop here
+  // for (let i = 0; i < completedTasks.length; i++){
+  //   if 
+  // }
+  let tempArray = completedTasks.map((task) => {return {date: task.completed!, 
+                                completedCount: 1}});
+  //console.log(tempArray);
+                        
+  for(let i = 0; i<tempArray.length; i++){
+    tasksArray.splice(tasksArray.length - indexSub, 0, tempArray[i]);
+    tasksArray[tasksArray.length - indexSub].completedCount! =  tasksArray[tasksArray.length-indexSub].completedCount! + tasksArray[tasksArray.length-(indexSub + 1)].completedCount!;
+  };
+  //const test = tasksArray.concat(hello);
+  //completedTasks.push({})
+  //console.log(completedTasks);
+  //console.log(tasksArray);
+  //console.log(completedTasks.map((task) => {return {date: task.completed, completedCount: completedNo + 1}}));
+  //console.log(hello);
+  //console.log(taskList);
+  //console.log(taskList.map((task) => {return {taskTitle: task.title, taskAssignee: task.assignee}} ));
 
   return (
-    <Card>
+    <Card className="@container/card">
       <CardHeader>
-        <CardTitle>{project?.title}</CardTitle>
-        <CardDescription>{project?.posted} - {project?.completed}</CardDescription>
+        <CardTitle>Project: {project?.title}</CardTitle>
+        <CardDescription>{project?.posted} - {project?.deadline}</CardDescription>
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig}>
           <LineChart
             accessibilityLayer
-            data={taskList.map((task) => {return {title: task.title, assignee: task.assignee}} )}
+            data={tasksArray}
             margin={{
-              left: 12,
+              left: 25,
               right: 12,
             }}
           >
             <CartesianGrid vertical={false} />
+            {/* <Line
+              data={thisTest}
+              dataKey="total"
+              type="linear"
+              connectNulls={true}
+              dot={false}
+            /> */}
             <XAxis
-              dataKey="title"
+              dataKey="date"
               tickLine={false}
-              axisLine={false}
+              axisLine={true}
               tickMargin={8}
-              tickFormatter={(value) => value.slice(0, 3)}
+              tickFormatter={(value) => value}
             />
+            {/* <YAxis 
+              tickMargin={8}
+              tickLine={false} 
+              axisLine={false} 
+              domain={[0, taskList.length + 1]} /> */}
             <ChartTooltip
               cursor={false}
               content={<ChartTooltipContent hideLabel />}
             />
             <Line
-              dataKey="assignee"
-              type="natural"
-              stroke="var(--color-desktop)"
+              label="Completed tasks"
+              dataKey="completedCount"
+              type="bump"
+              stroke={completedColour}
+              color= {completedColour}
               strokeWidth={2}
               dot={false}
+              connectNulls={false}
+            />
+            <ReferenceLine 
+            x={thisTest[0].date!}
+            stroke="#6f1e1eff"
+            strokeWidth={2}
             />
           </LineChart>
         </ChartContainer>
       </CardContent>
+      <CardFooter>
+          <CardDescription>{project?.completed !== null ? "This project has been completed." : "This project is in progress."}</CardDescription>
+      </CardFooter>
     </Card>
   )
 }
