0b2391b4432e15ce290048da2d8e014118f1aca2
fireyss <133407801+fireyss@users.noreply.github.com>
2025-11-02 22:20:24 +0000

    fixed refresh

--- frontend/src/routes/ProtectedRoute.tsx
+++ frontend/src/routes/ProtectedRoute.tsx
@@ -2,8 +2,24 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "@/context/AuthContext";
 
+import { Spinner } from "@/components/ui/shadcn-io/spinner";
+
 export default function ProtectedRoute() {
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
+
+    if (loading) {
+        return (
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100vh',
+            }}>
+                <Spinner />
+            </div>
+        );
+
+    }
 
     if (!user) {
         return <Navigate to="/" replace />;
